c46c83c64ab3b5dddda5c0392d4e88c476537717
Minh Duc Pham <minhduc99@outlook.com>
2026-01-03 14:19:25 -0500

    Initial Payload CMS project

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+import { withPayload } from '@payloadcms/next/withPayload';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+    images: {
+        remotePatterns: [{
+            protocol: 'http',
+            hostname: '127.0.0.1',
+            port: '8080',
+        }]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/admin',
+                destination: '/admin/collections/artists',
+                permanent: false,
+            },
+        ]
+    },
+};
+
+export default withPayload(nextConfig);
